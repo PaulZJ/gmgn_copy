603df16c9128423639dc6a679defa312df3faeb9
PaulZJ <PaulZJ@users.noreply.github.com>
2025-06-22 04:27:18 +0000

    deploy: 46830a6eda331518b1cc11d152fba45ee300e394

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2882126862"
+    serviceWorkerVersion: "386248551"
   }
 });
